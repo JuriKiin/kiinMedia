7d359b8abe27c7da107cb3c00470349a8ffcde39
Juri Kiin <cornfield24@gmail.com>
2016-10-05 23:35:45 -0400

    - Began to add content to profile section of home page - created links within the organization boxes in index.html to bring the user to the correct project - Added a new js method in tab.js that can open a url in the same window (Can be put on any element) - Added extra divs to help organize the code - Added some documentation in the HTML and CSS to make it easier to understand what's going on

---
+++ JS/tab.js
@@ -0,0 +1,11 @@
+function openInNewTab(url)
+{
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+function openInSameTab(url)
+{
+    var win = window.open(url,"_self");
+    win.fous();
+
+}
